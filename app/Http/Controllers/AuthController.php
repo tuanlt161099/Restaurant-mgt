994daf0b807595ad559e2e5a7e2b6f6a68e23011
Tuanlt161099 <tuanlt161099@gmail.com>
2021-08-12 20:24:28 +0700

    AuthController sanctum token

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Http\Requests\AuthRequest;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(AuthRequest $request){
+        $fields =  $request->validated();
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
+
+        // $token = $user->createToken('myapptoken')->plainTextToken;
+
+        // $data = [
+        //     'user' => $user,
+        //     'token' => $token,
+        // ];
+        $response = [
+            "status" => "success",
+            "data" => [],
+            "message" => "Registered success"
+        ];
+
+        return response($response, 201);
+    }
+
+
+    public function logout(Request $request){
+        auth()->user()->tokens()->delete();
+        return [
+            'message' => "Logged out"
+        ];
+    }
+
+    public function login(Request $request){
+        $fields = $request->validate([
+            //unique users table and email field
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+       //check email
+        $user = User::where('email',$fields['email'])->first();
+        if(!$user){
+             return response([
+                 "status" =>"failed",
+                'message' =>"Email not existed"
+            ],401);
+        }
+
+        if( !Hash::check($fields['password'], $user->password)){
+            return response([
+                "status" =>"failed",
+                'message' =>"Password was incorrect"
+            ],401);
+        };
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+        $data = [ 
+            'user' => $user,
+            'token' => $token,
+        ];
+        $response = [
+            "status" =>"success",
+            "data" => $data,
+            "message" => "Logging successfully"
+        ];
+
+        return response($response, 201);
+    }
+
+}
