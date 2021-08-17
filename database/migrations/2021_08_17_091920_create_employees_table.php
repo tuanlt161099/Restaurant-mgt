34c3b9c93235aca9de1508e20b015fb9ea14a993
VuTien-creator <tienlv304@gmail.com>
2021-08-17 22:05:39 +0700

    some bug

--- database/migrations/2021_08_17_091920_create_employees_table.php
+++ database/migrations/2021_08_17_091920_create_employees_table.php
@@ -13,7 +13,6 @@ class CreateEmployeesTable extends Migration
      */
     public function up()
     {
-<<<<<<< HEAD:database/migrations/2021_08_13_024157_create_bills_table.php
         Schema::create('bills', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('user_id')->unsigned()->nullable();
@@ -22,15 +21,6 @@ class CreateEmployeesTable extends Migration
             $table->foreign('table_id')->references('id')->on('tables')->onUpdate('cascade')->onDelete('cascade');
             $table->float('total');
             $table->float('discount');
-=======
-        Schema::create('employees', function (Blueprint $table) {
-            $table->id();
-            $table->bigInteger('user_id')->unsigned()->nullable();
-            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
-            $table->string('name');
-            $table->string('user_name');
-            $table->string('password');
->>>>>>> dd0482b3b9ec825bc95fac72b9983deda6948f62:database/migrations/2021_08_17_091920_create_employees_table.php
             $table->timestamps();
         });
     }
